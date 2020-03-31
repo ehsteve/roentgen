b49eabfa9ce542b780a1b42d5c37579c8b8901d3
Steven Christe <steven.christe@nasa.gov>
2020-03-31 16:16:11 -0400

    test remove files

--- roentgen/conftest.py
+++
@@ -1,7 +0,0 @@
-# This file is used to configure the behavior of pytest when using the Astropy
-# test infrastructure.
-import os
-import pytest
-import astropy.units as u
-
-packagename = os.path.basename(os.path.dirname(__file__))
