07c1864fdf58ee1ee03a1c543e37bcccc8d2a0d6
Steven Christe <ehsteve@users.noreply.github.com>
2023-01-05 14:14:04 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,14 +9,15 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'roentgen'
-copyright = '2020, '
-author = 'Steven D. Christe, Shane Maloney, Dan Ryan'
+project = "roentgen"
+copyright = "2023, Public Domain"
+author = "Steven D. Christe, Nabil Freij, Shane Maloney, Daniel Ryan"
 
 # The full version, including alpha/beta/rc tags
 from roentgen import __version__
+
 release = __version__
-is_development = '.dev' in __version__
+is_development = ".dev" in __version__
 
 # -- General configuration ---------------------------------------------------
 
@@ -24,19 +25,19 @@ is_development = '.dev' in __version__
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.inheritance_diagram',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.doctest',
-    'sphinx.ext.mathjax',
-    'sphinx_automodapi.automodapi',
-    'sphinx_automodapi.smart_resolver',
-    'matplotlib.sphinxext.plot_directive',
-    'pytest_doctestplus.sphinx.doctestplus'
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.inheritance_diagram",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.doctest",
+    "sphinx.ext.mathjax",
+    "sphinx_automodapi.automodapi",
+    "sphinx_automodapi.smart_resolver",
+    "matplotlib.sphinxext.plot_directive",
+    "pytest_doctestplus.sphinx.doctestplus",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -45,40 +46,49 @@ extensions = [
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents. Set to the "smart" one.
-default_role = 'obj'
+default_role = "obj"
 
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/',
-               (None, 'http://data.astropy.org/intersphinx/python3.inv')),
-    'numpy': ('https://docs.scipy.org/doc/numpy/',
-              (None, 'http://data.astropy.org/intersphinx/numpy.inv')),
-    'scipy': ('https://docs.scipy.org/doc/scipy/reference/',
-              (None, 'http://data.astropy.org/intersphinx/scipy.inv')),
-    'matplotlib': ('https://matplotlib.org/',
-                   (None, 'http://data.astropy.org/intersphinx/matplotlib.inv')),
-    'astropy': ('http://docs.astropy.org/en/stable/', None),
-    'sunpy': ('https://docs.sunpy.org/en/stable/', None)}
+    "python": (
+        "https://docs.python.org/3/",
+        (None, "http://data.astropy.org/intersphinx/python3.inv"),
+    ),
+    "numpy": (
+        "https://docs.scipy.org/doc/numpy/",
+        (None, "http://data.astropy.org/intersphinx/numpy.inv"),
+    ),
+    "scipy": (
+        "https://docs.scipy.org/doc/scipy/reference/",
+        (None, "http://data.astropy.org/intersphinx/scipy.inv"),
+    ),
+    "matplotlib": (
+        "https://matplotlib.org/",
+        (None, "http://data.astropy.org/intersphinx/matplotlib.inv"),
+    ),
+    "astropy": ("http://docs.astropy.org/en/stable/", None),
+    "sunpy": ("https://docs.sunpy.org/en/stable/", None),
+}
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_logo = 'logo/roentgen.svg'
+html_logo = "logo/roentgen.svg"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -86,13 +96,13 @@ html_logo = 'logo/roentgen.svg'
 # html_static_path = ['_static']
 
 # Render inheritance diagrams in SVG
-#graphviz_output_format = "svg"
+# graphviz_output_format = "svg"
 
-#graphviz_dot_args = [
+# graphviz_dot_args = [
 #    '-Nfontsize=10',
 #    '-Nfontname=Helvetica Neue, Helvetica, Arial, sans-serif',
 #    '-Efontsize=10',
 #    '-Efontname=Helvetica Neue, Helvetica, Arial, sans-serif',
 #    '-Gfontsize=10',
 #    '-Gfontname=Helvetica Neue, Helvetica, Arial, sans-serif'
-#]
+# ]
